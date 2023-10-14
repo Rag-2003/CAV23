95808252b8b771f72d94f92119b7e1ccd1d02f3f
Rag-2003 <106827930+Rag-2003@users.noreply.github.com>
2023-10-14 22:43:21 +0530

    qwerty

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,16 @@
+import { SignIn } from "@clerk/nextjs";
+import { Sign } from "crypto";
+ 
+export default function Page() {
+  return (
+
+<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+
+    <SignIn />; 
+</div>
+  )
+  
+  
+  
+
+}
